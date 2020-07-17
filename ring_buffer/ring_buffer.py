5f99e137f65cdc58450618d630719e8904228940
Angelg10 <angelguzman97@gmail.com>
2020-07-17 13:17:06 -0400

    ring buffer, names, mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,9 +2,17 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
+        self.old = 0
 
     def append(self, item):
-        self.data.append(item)
+        if len(self.data) == self.capacity:
+            self.data[self.old] = item
+            if self.old + 1 == self.capacity:
+                self.old = 0
+            else:
+                self.old += 1
+        else:
+            self.data.append(item)
 
     def get(self):
         return self.data
